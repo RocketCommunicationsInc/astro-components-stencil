13ff81056049fb31d0696e0cf7fa674eb3240076
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2021-04-28 10:20:38 -0600

    figure out how to generate css files else where

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,11 +6,27 @@ module.exports = {
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-readme'],
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
-      test: /\.scss$/,
-      loaders: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      test: /\.s?css$/,
+      loaders: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '../',
+          },
+        },
+        'css-loader',
+        'sass-loader',
+      ],
       include: path.resolve(__dirname, '../scss'),
     });
 
+    /*
+    if one want to place css files elsewhere push additional instance of MiniCssExtractPlugin
+    with desired file location
+    * Note: files will only be generated during production build
+
+    config.plugins.push(new MiniCssExtractPlugin({ filename: '../static/styles/[name].css' }));
+    */
     config.plugins.push(new MiniCssExtractPlugin({ filename: '[name].css' }));
 
     return config;
