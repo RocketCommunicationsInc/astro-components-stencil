954716878e51de7a7df99e1ba76eb9e5ae21534a
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2021-05-04 16:21:45 -0600

    add readme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/web-components';
+import { addReadme } from 'storybook-readme/html';
+
+addDecorator(addReadme);
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   layout: 'fullscreen',
