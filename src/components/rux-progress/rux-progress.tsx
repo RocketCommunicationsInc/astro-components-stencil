f3c5f56304e1ab9762755a7cd183e28b9e2205bd
Micah Jones <micahjones@Micahs-MacBook-Pro.local>
2021-05-17 16:14:14 -0600

    copied css, set props, set getProgressAsString function

--- src/components/rux-progress/rux-progress.tsx
+++ src/components/rux-progress/rux-progress.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop, Method } from '@stencil/core';
 
 @Component({
   tag: 'rux-progress',
@@ -6,13 +6,24 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class RuxProgress {
+@Prop() maxvalue: number = 100;
+@Prop() value: number;
+@Prop() hidelabel: boolean = false;
 
+getProgressAsString(){
+  return this.maxvalue === 100 ? `${this.value}%` : `${this.value}/${this.maxvalue}`
+}
   render() {
     return (
       <Host>
+        <div class="rux-progress">
+            
+        </div>
         <slot></slot>
       </Host>
     );
   }
 
 }
+
+//? Gonna need to determine things with the given props, such as hidden, value, that kinda thing.
