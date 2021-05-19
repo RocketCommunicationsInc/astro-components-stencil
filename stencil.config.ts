b995bb86bb300b1f15344c1536a7bfb959c234ea
Joel Perez <joel.perez@rocketcom.com>
2021-05-19 15:34:55 -0600

    removed conditional stylesheet theme feature

--- stencil.config.ts
+++ stencil.config.ts
@@ -6,8 +6,6 @@ import { inlineSvg } from 'stencil-inline-svg';
 export const config: Config = {
   namespace: 'astro-web-components',
   globalStyle: 'src/global/global.scss',
-  // For theme integration
-  globalScript: 'src/global/global.ts',
   outputTargets: [
     {
       type: 'dist',
