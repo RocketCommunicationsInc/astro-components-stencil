7f76aab8d596d161cb2f1feffe1011eec785b237
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-05-21 16:04:48 -0400

    style: remove ts-ignore

--- src/components/rux-clock/rux-clock.tsx
+++ src/components/rux-clock/rux-clock.tsx
@@ -81,8 +81,7 @@ export class RuxClock {
 
     this.updateTime();
 
-    // @ts-ignore
-    this._timer = setInterval(() => {
+    this._timer = window.setInterval(() => {
       this.updateTime();
     }, 1000);
   }
@@ -96,7 +95,7 @@ export class RuxClock {
     this.dayOfYear = getDayOfYear(zonedTimeToUtc(new Date(), this._timezone));
   }
 
-  private convertTimezone(timezone) {
+  private convertTimezone(timezone: string) {
     this._timezone = this.militaryTimezones[timezone.toUpperCase()];
     this.tzFormat = 'O';
     if (!this._timezone) {
