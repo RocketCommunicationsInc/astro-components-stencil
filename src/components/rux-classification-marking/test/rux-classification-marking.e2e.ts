d00f701b002b3d5c5a8beb26b715b96e6c309674
NicDominguez <nic.dominguez@rocketcom.com>
2021-05-21 09:06:42 -0700

    corrects component nameing

--- src/components/rux-classification-marking/test/rux-classification-marking.e2e.ts
+++ src/components/rux-classification-marking/test/rux-classification-marking.e2e.ts
@@ -1,11 +1,11 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('rux-classification-markings', () => {
+describe('rux-classification-marking', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<rux-classification-markings></rux-classification-markings>');
+    await page.setContent('<rux-classification-marking></rux-classification-marking>');
 
-    const element = await page.find('rux-classification-markings');
+    const element = await page.find('rux-classification-marking');
     expect(element).toHaveClass('hydrated');
   });
 });
