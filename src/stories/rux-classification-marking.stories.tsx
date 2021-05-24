a2ecb448202ea3f84fec3b38b077eeda74f322c7
NicDominguez <nic.dominguez@rocketcom.com>
2021-05-24 11:07:06 -0700

    makes paragraph margin an inline style in the story

--- src/stories/rux-classification-marking.stories.tsx
+++ src/stories/rux-classification-marking.stories.tsx
@@ -115,7 +115,7 @@ export const ClassificationMarkingTags = () => html`
     <div style="display: flex; flex-flow: column; justify-content: flex-start; width: 400px; margin:60px auto;">	  
       
       <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Top Secret//SCI</p>
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9; margin: 0 0 1rem 0;">Top Secret//SCI</p>
         <rux-classification-marking
           tag
           classification="top-secret-sci"
@@ -123,35 +123,35 @@ export const ClassificationMarkingTags = () => html`
       </div>
 
       <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Top Secret</p>	
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9; margin: 0 0 1rem 0;">Top Secret</p>	
         <rux-classification-marking
           tag
           classification="top-secret"
         ></rux-classification-marking>
       </div>
       <div style="display: flex; align-items:baseline;  position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Secret</p>	
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9; margin: 0 0 1rem 0;">Secret</p>	
         <rux-classification-marking
           tag
           classification="secret"
         ></rux-classification-marking>
       </div>
       <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Confidential</p>	
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9; margin: 0 0 1rem 0;">Confidential</p>	
         <rux-classification-marking
           tag
           classification="confidential"
         ></rux-classification-marking>
       </div>
       <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Controlled Unclassified</p>	
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9; margin: 0 0 1rem 0;">Controlled Unclassified</p>	
         <rux-classification-marking
           tag
           classification="controlled"
         ></rux-classification-marking>
       </div>
       <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-        <p style="display: flex; width:225px; font-size:13px; font-style:italic; color:#d5d7d9;">Uncontrolled Unclassified</p>	
+        <p style="display: flex; width:225px; font-size:13px; font-style:italic; color:#d5d7d9; margin: 0 0 1rem 0;">Uncontrolled Unclassified</p>	
         <rux-classification-marking
           tag
           classification="unclassified"
