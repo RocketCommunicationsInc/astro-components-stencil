950ddde113ac55a337c24cc031f77431e79e6c44
Joel Perez <joel.perez@rocketcom.com>
2021-05-27 11:00:05 -0600

    removed function as it is now a shared utility function

--- src/components/rux-monitoring-icon/rux-monitoring-icon.tsx
+++ src/components/rux-monitoring-icon/rux-monitoring-icon.tsx
@@ -1,5 +1,6 @@
 import { Component, h, Prop, Watch } from '@stencil/core';
 import { Status } from '../../common/commonTypes.module'
+import { collapseNotifications } from '../../utils/utils'
 
 @Component({
   tag: 'rux-monitoring-icon',
@@ -8,7 +9,8 @@ import { Status } from '../../common/commonTypes.module'
 })
 export class RuxMonitoringIcon {
   /*
-  * Styles the icon according to the Astro Status colors. Valid options are the Astro statuses `critical`, `serious`, `caution`, `normal`, `standby` and `off`
+  * Styles the icon according to the Astro Status colors. 
+  * Valid options are the Astro statuses `critical`, `serious`, `caution`, `normal`, `standby` and `off`
   */
   @Prop({reflect: true}) status: Status = 'normal';
   /*
@@ -20,41 +22,16 @@ export class RuxMonitoringIcon {
   */
   @Prop() sublabel: string;
   /*
-  * Displays an Astro icon matching this string. For a [full list of available icons, see the Icons section in Astro UXDS Guidelines](https://astrouxds.com/ui-components/icons-and-symbols)
+  * Displays an Astro icon matching this string. For a [full list of available icons, 
+  * see the Icons section in Astro UXDS Guidelines](https://astrouxds.com/ui-components/icons-and-symbols)
   */
   @Prop() icon: string;
   /*
-  * If provided and greater than `0`, displays an outlined number badge at the bottom right of the icon. Numbers above `9999` are abbreviated to `'10K'` or `'100K'` for numbers in the thousands, `'1.5M'` for millions, and `'1.5B'` for billions. The badge uses `'∞'` for one trillion or higher.
+  * If provided and greater than `0`, displays an outlined number badge at the bottom right of the icon. 
+  * Numbers above `9999` are abbreviated to `'10K'` or `'100K'` for numbers in the thousands, 
+  * `'1.5M'` for millions, and `'1.5B'` for billions. The badge uses `'∞'` for one trillion or higher.
   */
   @Prop() notifications: number = 0;
-
-  _collapseNotification(value: number) {
-    const n = Math.floor(value);
-
-    // don't show any values less than 0
-    if (n <= 0) return null;
-
-    // get the place value
-    const thousand = Math.floor((n / 1000) % 1000); // only return a whole number
-    const million = (n / 1000000) % 1000000; // return a decimal value for numbers like 1.2m
-    const billion = (n / 1000000000) % 1000000000; // return a decimal value for numbers like 1.2b
-    const trillion = (n / 1000000000000) % 1000000000000; // trillion is just to offer an overflow instance
-
-    // set the display to its original state
-    let _shorthand = n.toString();
-
-    if (trillion >= 1) {
-      _shorthand = '∞';
-    } else if (billion >= 1) {
-      _shorthand = `${billion.toFixed(1).toString()}B`;
-    } else if (million >= 1) {
-      _shorthand = `${million.toFixed(1).toString()}M`;
-    } else if (thousand >= 1) {
-      _shorthand = `${thousand}K`;
-    }
-
-    return _shorthand;
-  }
 
   @Watch('status')
   validateStatus(newValue: string) {
@@ -78,7 +55,7 @@ export class RuxMonitoringIcon {
   private _badgeTemplate() {
     return (
       <div class={`rux-advanced-status__badge ${!this.notifications && 'rux-advanced-status__hidden'}`}>
-        {this._collapseNotification(this.notifications)}
+        {collapseNotifications(this.notifications)}
       </div>
     )
   }
