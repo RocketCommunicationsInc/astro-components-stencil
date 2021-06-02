16103821063e63221b8735d1ba6244640c154a4c
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-06-02 18:34:16 -0400

    fix merge conflict

--- src/components/rux-clock/rux-clock.tsx
+++ src/components/rux-clock/rux-clock.tsx
@@ -9,61 +9,6 @@ import { militaryTimezones } from './military-timezones'
     shadow: true,
 })
 export class RuxClock {
-<<<<<<< HEAD
-    private _timezone: string = 'UTC'
-    private tzFormat: string = 'z'
-    private _timer: number
-    private militaryTimezones = {
-        A: '+01:00',
-        B: '+02:00',
-        C: '+03:00',
-        D: '+04:00',
-        E: '+05:00',
-        F: '+06:00',
-        G: '+07:00',
-        H: '+08:00',
-        I: '+09:00',
-        K: '+10:00',
-        L: '+11:00',
-        M: '+12:00',
-        N: '-01:00',
-        O: '-02:00',
-        P: '-03:00',
-        Q: '-04:00',
-        R: '-05:00',
-        S: '-06:00',
-        T: '-07:00',
-        U: '-08:00',
-        V: '-09:00',
-        W: '-10:00',
-        X: '-11:00',
-        Y: '-12:00',
-        Z: '+00:00',
-    }
-
-    @Prop() aos: number
-    @Prop() los: string
-    @Prop({ mutable: true }) timezone: string = 'UTC'
-    @Prop() hideTimezone: boolean = false
-    @Prop() hideDate: boolean = false
-    @Prop() small: boolean
-    @Prop({ mutable: true }) time: string
-    @Prop({ mutable: true }) dayOfYear: number
-
-    @Watch('timezone')
-    watchHandler() {
-        this.convertTimezone(this.timezone)
-    }
-
-    connectedCallback() {
-        this._timezone = this.timezone
-
-        this.updateTime()
-
-        // @ts-ignore
-        this._timer = setInterval(() => {
-            this.updateTime()
-=======
     private _timer: number
     private _timezone: string = 'UTC'
     private dayOfYear: number
@@ -119,7 +64,6 @@ export class RuxClock {
 
         this._timer = window.setInterval(() => {
             this._updateTime()
->>>>>>> next
         }, 1000)
     }
 
@@ -127,21 +71,6 @@ export class RuxClock {
         clearTimeout(this._timer)
     }
 
-<<<<<<< HEAD
-    private updateTime() {
-        this.time = format(
-            utcToZonedTime(new Date(), this._timezone),
-            `HH:mm:ss ${this.hideTimezone ? '' : this.tzFormat}`,
-            { timeZone: this._timezone }
-        )
-        this.dayOfYear = getDayOfYear(
-            zonedTimeToUtc(new Date(), this._timezone)
-        )
-    }
-
-    private convertTimezone(timezone) {
-        this._timezone = this.militaryTimezones[timezone.toUpperCase()]
-=======
     formatTime(time: Date, timezone: string): string {
         return format(
             utcToZonedTime(time, timezone),
@@ -159,7 +88,6 @@ export class RuxClock {
 
     convertTimezone(timezone: string) {
         this._timezone = militaryTimezones[timezone.toUpperCase()]
->>>>>>> next
         this.tzFormat = 'O'
         if (!this._timezone) {
             this._timezone = timezone
@@ -167,88 +95,6 @@ export class RuxClock {
         } else if (timezone.toUpperCase() == 'Z') {
             this.tzFormat = 'X'
         }
-<<<<<<< HEAD
-    }
-
-    render() {
-        return (
-            <Host>
-                <div class="rux-clock__segment rux-clock__day-of-the-year">
-                    <div
-                        class="rux-clock__segment__value"
-                        aria-labelledby="rux-clock__day-of-year-label"
-                    >
-                        {this.dayOfYear}
-                    </div>
-                    <div
-                        class="rux-clock__segment__label"
-                        id="rux-clock__day-of-year-label"
-                    >
-                        Date
-                    </div>
-                </div>
-                <div class="rux-clock__segment rux-clock__time">
-                    <div
-                        class="rux-clock__segment__value"
-                        aria-labelledby="rux-clock__time-label"
-                    >
-                        {this.time}
-                    </div>
-                    <div
-                        class="rux-clock__segment__label"
-                        id="rux-clock__time-label"
-                    >
-                        Time
-                    </div>
-                </div>
-
-                {this.aos ? (
-                    <div class="rux-clock__segment rux-clock__segment--secondary rux-clock__aos">
-                        <div
-                            class="rux-clock__segment__value"
-                            aria-labelledby="rux-clock__time-label--aos"
-                        >
-                            {format(
-                                utcToZonedTime(this.aos, this._timezone),
-                                'HH:mm:ss'
-                            )}
-                        </div>
-                        <div
-                            class="rux-clock__segment__label"
-                            id="rux-clock__time-label--aos"
-                        >
-                            AOS
-                        </div>
-                    </div>
-                ) : (
-                    ''
-                )}
-
-                {this.los ? (
-                    <div class="rux-clock__segment rux-clock__segment--secondary rux-clock__los">
-                        <div
-                            class="rux-clock__segment__value"
-                            aria-labelledby="rux-clock__time-label--los"
-                        >
-                            {format(
-                                utcToZonedTime(this.los, this._timezone),
-                                'HH:mm:ss'
-                            )}
-                        </div>
-                        <div
-                            class="rux-clock__segment__label"
-                            id="rux-clock__time-label--los"
-                        >
-                            LOS
-                        </div>
-                    </div>
-                ) : (
-                    ''
-                )}
-            </Host>
-        )
-    }
-=======
     }
 
     render() {
@@ -336,5 +182,4 @@ export class RuxClock {
             </Host>
         )
     }
->>>>>>> next
 }
