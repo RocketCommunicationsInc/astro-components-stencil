bedbef2e8ff132b844f13e1b28f75deb35bafa42
NicDominguez <nic.dominguez@rocketcom.com>
2021-06-02 14:30:44 -0700

    feat: adds validation to menuIcon prop and placeholder app state elements

--- src/components/rux-global-status-bar/rux-global-status-bar.tsx
+++ src/components/rux-global-status-bar/rux-global-status-bar.tsx
@@ -1,7 +1,25 @@
-import { Prop, Component, Host, h } from '@stencil/core';
+import { Prop, Component, Host, h, Watch } from '@stencil/core';
 import { AppName } from '../../common/functional-components/appName'
 import { AppVersion } from '../../common/functional-components/appVersion'
 import { AppDomain } from '../../common/functional-components/appDomain'
+import iconsJSON from '../../stories/rux-icons.json'
+
+//create single flat array of icon names from the rux-icons.json file
+let iconList = []
+function setIconList() {
+  const iconsObject = iconsJSON.solid
+  const iconCategories = (Object.keys(iconsJSON.solid))
+
+  iconCategories.map((category) => {
+    const iconsArray = iconsObject[category]
+    const flatIconsArray = iconsArray.map((icon) => {
+      return icon.icon
+    })
+    iconList.push(...flatIconsArray)
+  })
+}
+setIconList()
+
 
 @Component({
   tag: 'rux-global-status-bar',
@@ -12,10 +30,18 @@ export class RuxGlobalStatusBar {
   @Prop() appName?: string;
   @Prop() appVersion?: string;
   @Prop() appDomain?: string;
-  // TODO: validate menuIcon prop to make sure it is a valid library icon
-  @Prop() menuIcon?: string = "apps";
+  @Prop({mutable: true}) menuIcon?: string = "apps";
+
+  @Watch('menuIcon')
+  validateMenuIcon(requestedMenuIcon: string, prevMenuIcon: string) {
+    if (!iconList.includes(requestedMenuIcon)) {
+      this.menuIcon = prevMenuIcon;
+      throw new Error('icon cannot be found in rux-icon library')
+    }
+  }
 
   render() {
+
     return (
       <Host>
         <header>
@@ -26,7 +52,9 @@ export class RuxGlobalStatusBar {
           <slot name="app-meta">
             {/* app=meta div content is fallback component when app-meta slot is unfilled */}
             <div class="app-meta">
-              <AppDomain domain={this.appDomain} /><AppName name={this.appName.toUpperCase()}><AppVersion version={this.appVersion} /></ AppName>
+              <div class="app-info-wrapper"><AppDomain domain={this.appDomain} /><AppName name={this.appName.toUpperCase()}><AppVersion version={this.appVersion} /></ AppName></div>
+              <div class="temp-app-state">App state</div>
+              <div class="temp-user-name">User Name</div>
             </div>
           </slot>
         <slot></slot>
