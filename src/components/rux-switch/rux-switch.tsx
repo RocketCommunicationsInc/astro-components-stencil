b58c3191a4734d94eefffe40fdee1f47c8de68e0
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-06-02 18:21:02 -0400

    feat: props

--- src/components/rux-switch/rux-switch.tsx
+++ src/components/rux-switch/rux-switch.tsx
@@ -1,22 +1,30 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Prop, Host, h } from '@stencil/core'
 
 @Component({
-  tag: 'rux-switch',
-  styleUrl: 'rux-switch.css',
-  shadow: true,
+    tag: 'rux-switch',
+    styleUrl: 'rux-switch.css',
+    shadow: true,
 })
 export class RuxSwitch {
+    @Prop() value?: string | null = 'on'
+    @Prop() name: string
 
-  render() {
-    return (
-      <Host>
-        <div class="rux-switch">
-        <input class="rux-switch__input" type="checkbox"/>
-        <label class="rux-switch__button" htmlFor="${this._id}">
-        </label>
-      </div>
-      </Host>
-    );
-  }
-
+    render() {
+        return (
+            <Host>
+                <div class="rux-switch">
+                    <input
+                        class="rux-switch__input"
+                        value={this.value}
+                        type="checkbox"
+                        name={this.name}
+                    />
+                    <label
+                        class="rux-switch__button"
+                        htmlFor="${this._id}"
+                    ></label>
+                </div>
+            </Host>
+        )
+    }
 }
