f6b28ee9cdcad1bacbfbaa8cf718b1bc600ffc17
NicDominguez <nic.dominguez@rocketcom.com>
2021-06-02 15:49:41 -0700

    refactor: better class code structure

--- src/components/rux-global-status-bar/rux-global-status-bar.tsx
+++ src/components/rux-global-status-bar/rux-global-status-bar.tsx
@@ -5,20 +5,8 @@ import { AppDomain } from '../../common/functional-components/appDomain'
 import iconsJSON from '../../stories/rux-icons.json'
 
 //create single flat array of icon names from the rux-icons.json file
-let iconList = []
-function setIconList() {
-  const iconsObject = iconsJSON.solid
-  const iconCategories = (Object.keys(iconsJSON.solid))
 
-  iconCategories.map((category) => {
-    const iconsArray = iconsObject[category]
-    const flatIconsArray = iconsArray.map((icon) => {
-      return icon.icon
-    })
-    iconList.push(...flatIconsArray)
-  })
-}
-setIconList()
+
 
 
 @Component({
@@ -30,18 +18,36 @@ export class RuxGlobalStatusBar {
   @Prop() appName?: string;
   @Prop() appVersion?: string;
   @Prop() appDomain?: string;
-  @Prop({mutable: true}) menuIcon?: string = "apps";
+  @Prop({ mutable: true, reflect: true }) menuIcon?: string = "apps";
+  iconList = []
+
+  connectedCallback(){
+    this.setIconList()
+  }
 
   @Watch('menuIcon')
   validateMenuIcon(requestedMenuIcon: string, prevMenuIcon: string) {
-    if (!iconList.includes(requestedMenuIcon)) {
+    if (!this.iconList.includes(requestedMenuIcon)) {
       this.menuIcon = prevMenuIcon;
       throw new Error('icon cannot be found in rux-icon library')
     }
   }
+  
+  // creates a flattened array of all icon names in rux-icons.json
+  setIconList() {
+    const iconsObject = iconsJSON.solid
+    const iconCategories = (Object.keys(iconsJSON.solid))
+
+    iconCategories.map((category) => {
+      const iconsArray = iconsObject[category]
+      const flatIconsArray = iconsArray.map((icon) => {
+        return icon.icon
+      })
+      this.iconList.push(...flatIconsArray)
+    })
+  }
 
   render() {
-
     return (
       <Host>
         <header>
@@ -52,7 +58,12 @@ export class RuxGlobalStatusBar {
           <slot name="app-meta">
             {/* app=meta div content is fallback component when app-meta slot is unfilled */}
             <div class="app-meta">
-              <div class="app-info-wrapper"><AppDomain domain={this.appDomain} /><AppName name={this.appName.toUpperCase()}><AppVersion version={this.appVersion} /></ AppName></div>
+              <div class="app-info-wrapper">
+                <AppDomain domain={this.appDomain} />
+                <AppName name={this.appName.toUpperCase()}>
+                  <AppVersion version={this.appVersion} />
+                </AppName>
+              </div>
               <div class="temp-app-state">App state</div>
               <div class="temp-user-name">User Name</div>
             </div>
