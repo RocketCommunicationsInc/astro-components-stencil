c9fafb830b2e39af1b9df3e5c1053b1a0ef84e6d
NicDominguez <nic.dominguez@rocketcom.com>
2021-06-07 15:22:44 -0700

    fix: updates story with new rux-tabs

--- src/stories/rux-global-status-bar.stories.tsx
+++ src/stories/rux-global-status-bar.stories.tsx
@@ -73,32 +73,38 @@ export const GlobalStatusBarWithTabs = () => {
   const menuIconKnob = text('Menu Icon', 'apps');
 
     return html`
-    <div style="display: flex; justify-content: center;">
+    <div style="display: flex; justify-content: center; flex-wrap: wrap;">
         <rux-global-status-bar class="dark-theme" .appDomain="${appDomainKnob}" .appName="${appNameKnob}" .appVersion="${appVersionKnob}" .menuIcon="${menuIconKnob}">
             <rux-tabs id="tab-set-id-1">
-                <rux-tab id="tab-id-1-1">Tab 1</rux-tab>
-                <rux-tab id="tab-id-1-2">Tab 2</rux-tab>
-                <rux-tab id="tab-id-1-3">Tab 3</rux-tab>
+                <rux-tab id="tab-id-1">Tab 1</rux-tab>
+                <rux-tab id="tab-id-2">Tab 2</rux-tab>
+                <rux-tab id="tab-id-3">Tab 3</rux-tab>
             </rux-tabs>
-            <rux-button>Emergency shut off</rux-button>
+            <rux-button slot="right-side" >Emergency shut off</rux-button>
         </rux-global-status-bar>
         <rux-tab-panels aria-labelledby="tab-set-id-1">
-            <rux-tab-panel aria-labelledby="tab-id-1-1">
-                <pre
-                    style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"
-                ><<span>!-- Tab 1 HTML content --</span>></pre>
+            <rux-tab-panel aria-labelledby="tab-id-1">
+                <div
+                    style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; font-family: monospace;"
+                >
+                    <pre><<span>!-- Tab 1 HTML content --</span>></pre>
+                </div>
             </rux-tab-panel>
-            <rux-tab-panel aria-labelledby="tab-id-1-2">
-                <pre
-                    style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"
-                ><<span>!-- Tab 2 HTML content --</span>></pre>
+            <rux-tab-panel aria-labelledby="tab-id-2">
+                <div
+                    style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; font-family: monospace;"
+                >
+                    <pre><<span>!-- Tab 2 HTML content --</span>></pre>
+                </div>
             </rux-tab-panel>
-            <rux-tab-panel aria-labelledby="tab-id-1-3">
-                <pre
-                    style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"
-                ><<span>!-- Tab 3 HTML content --</span>></pre>
+            <rux-tab-panel aria-labelledby="tab-id-3">
+                <div
+                    style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; font-family: monospace;"
+                >
+                    <pre><<span>!-- Tab 3 HTML content --</span>></pre>
+                </div>
             </rux-tab-panel>
-        </rux-tab-panels>
+        </rux-tab-panels> 
     </div>
     `
 }
