42779645c3f33f668c43397a453a541888d04bb8
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-06-18 13:52:40 -0400

    feat: prop definitions and documentation

--- src/components.d.ts
+++ src/components.d.ts
@@ -59,6 +59,52 @@ export namespace Components {
          */
         timezone: string
     }
+    interface RuxDatetime {
+        /**
+         * The date time to be formatted
+         */
+        date: Date | string
+        /**
+         * Format options for day
+         */
+        day?: 'numeric' | '2-digit'
+        /**
+         * Format options for hour
+         */
+        hour?: 'numeric' | '2-digit'
+        /**
+         * Display date in 12 hour time.
+         */
+        hour12: boolean
+        /**
+         * The locale
+         */
+        locale: string
+        /**
+         * Format options for minute
+         */
+        minute?: 'numeric' | '2-digit'
+        /**
+         * Format options for month
+         */
+        month?: 'numeric' | '2-digit' | 'narrow' | 'short' | 'long'
+        /**
+         * Format options for second
+         */
+        second?: 'numeric' | '2-digit'
+        /**
+         * Format options for Timezone
+         */
+        timeZone?: string
+        /**
+         * Format options for Timezone name
+         */
+        timeZoneName?: 'short' | 'long'
+        /**
+         * Format options for year
+         */
+        year?: 'numeric' | '2-digit'
+    }
     interface RuxGlobalStatusBar {
         /**
          * Sets the domain of the application to be displayed in the app-meta element
@@ -5509,6 +5555,13 @@ declare global {
     var HTMLRuxClockElement: {
         prototype: HTMLRuxClockElement
         new (): HTMLRuxClockElement
+    }
+    interface HTMLRuxDatetimeElement
+        extends Components.RuxDatetime,
+            HTMLStencilElement {}
+    var HTMLRuxDatetimeElement: {
+        prototype: HTMLRuxDatetimeElement
+        new (): HTMLRuxDatetimeElement
     }
     interface HTMLRuxGlobalStatusBarElement
         extends Components.RuxGlobalStatusBar,
@@ -12975,6 +13028,7 @@ declare global {
         'rux-button-group': HTMLRuxButtonGroupElement
         'rux-classification-marking': HTMLRuxClassificationMarkingElement
         'rux-clock': HTMLRuxClockElement
+        'rux-datetime': HTMLRuxDatetimeElement
         'rux-global-status-bar': HTMLRuxGlobalStatusBarElement
         'rux-icon': HTMLRuxIconElement
         'rux-icon-360': HTMLRuxIcon360Element
@@ -14093,6 +14147,52 @@ declare namespace LocalJSX {
          * Accepts the [IANA timezone string format](https://www.iana.org/time-zones) such as `'America/Los_Angeles'` or any single-character designation for a [military timezones](https://en.wikipedia.org/wiki/List_of_military_time_zones) (`'A'` through `'Z'`, excluding `'J'`), both case-insensitive. If no value for timezone is provided, the clock will use `'UTC'`. See [`toLocaleString()` on MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString#Parameters) for more details.
          */
         timezone?: string
+    }
+    interface RuxDatetime {
+        /**
+         * The date time to be formatted
+         */
+        date?: Date | string
+        /**
+         * Format options for day
+         */
+        day?: 'numeric' | '2-digit'
+        /**
+         * Format options for hour
+         */
+        hour?: 'numeric' | '2-digit'
+        /**
+         * Display date in 12 hour time.
+         */
+        hour12?: boolean
+        /**
+         * The locale
+         */
+        locale?: string
+        /**
+         * Format options for minute
+         */
+        minute?: 'numeric' | '2-digit'
+        /**
+         * Format options for month
+         */
+        month?: 'numeric' | '2-digit' | 'narrow' | 'short' | 'long'
+        /**
+         * Format options for second
+         */
+        second?: 'numeric' | '2-digit'
+        /**
+         * Format options for Timezone
+         */
+        timeZone?: string
+        /**
+         * Format options for Timezone name
+         */
+        timeZoneName?: 'short' | 'long'
+        /**
+         * Format options for year
+         */
+        year?: 'numeric' | '2-digit'
     }
     interface RuxGlobalStatusBar {
         /**
@@ -19528,6 +19628,7 @@ declare namespace LocalJSX {
         'rux-button-group': RuxButtonGroup
         'rux-classification-marking': RuxClassificationMarking
         'rux-clock': RuxClock
+        'rux-datetime': RuxDatetime
         'rux-global-status-bar': RuxGlobalStatusBar
         'rux-icon': RuxIcon
         'rux-icon-360': RuxIcon360
@@ -20608,6 +20709,8 @@ declare module '@stencil/core' {
                 JSXBase.HTMLAttributes<HTMLRuxClassificationMarkingElement>
             'rux-clock': LocalJSX.RuxClock &
                 JSXBase.HTMLAttributes<HTMLRuxClockElement>
+            'rux-datetime': LocalJSX.RuxDatetime &
+                JSXBase.HTMLAttributes<HTMLRuxDatetimeElement>
             'rux-global-status-bar': LocalJSX.RuxGlobalStatusBar &
                 JSXBase.HTMLAttributes<HTMLRuxGlobalStatusBarElement>
             'rux-icon': LocalJSX.RuxIcon &
