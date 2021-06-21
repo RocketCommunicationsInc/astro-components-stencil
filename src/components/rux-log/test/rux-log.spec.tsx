421ec47f2f75ca52586c57796e27143ee35440e9
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-06-21 14:13:52 -0400

    test: filtering

--- src/components/rux-log/test/rux-log.spec.tsx
+++ src/components/rux-log/test/rux-log.spec.tsx
@@ -1,18 +1,42 @@
 import { newSpecPage } from '@stencil/core/testing'
 import { RuxLog } from '../rux-log'
+import { LogRow } from '../rux-log.model'
 
 describe('rux-log', () => {
-    it('renders', async () => {
-        const page = await newSpecPage({
-            components: [RuxLog],
-            html: `<rux-log></rux-log>`,
-        })
-        expect(page.root).toEqualHtml(`
-      <rux-log>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </rux-log>
-    `)
+    it('builds', async () => {
+        const log = new RuxLog()
+        expect(log).toBeTruthy()
+    })
+
+    it('filters', async () => {
+        const logData = [
+            {
+                timestamp: new Date(1557503698781),
+                status: 'off',
+                message: 'Antenna DGS 1 went offline.',
+            },
+            {
+                timestamp: new Date(1557503698781),
+                status: 'serious',
+                message: 'USA-177 experienced solar panel misalignment.',
+            },
+            {
+                timestamp: new Date(1557503698781),
+                status: 'caution',
+                message: 'USA-168 suffered power degradation.',
+            },
+        ] as LogRow[]
+
+        const log = new RuxLog()
+        log.data = logData
+        log.filter = 'Antenna DGS'
+
+        expect(log.filteredData).toStrictEqual([
+            {
+                timestamp: new Date(1557503698781),
+                status: 'off',
+                message: 'Antenna DGS 1 went offline.',
+            },
+        ])
     })
 })
