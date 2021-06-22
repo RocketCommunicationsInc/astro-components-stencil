3a0b473e6d3a0ceb721ce38c341e3567ffcb7142
Joel Perez <joel.perez@rocketcom.com>
2021-06-22 17:01:12 -0600

    cleaned up comments and styles on pop up menu

--- src/components/rux-pop-up-menu/readme.md
+++ src/components/rux-pop-up-menu/readme.md
@@ -7,11 +7,11 @@
 
 ## Properties
 
-| Property    | Attribute | Description                                                                                                                 | Type          | Default     |
-| ----------- | --------- | --------------------------------------------------------------------------------------------------------------------------- | ------------- | ----------- |
-| `anchorEl`  | --        | Element to anchor the menu to. If none is given the menu will anchor to the trigger element where aria-controls === menu id | `HTMLElement` | `undefined` |
-| `open`      | `open`    | Boolean which controls when to show the menu                                                                                | `boolean`     | `false`     |
-| `triggerEl` | --        |                                                                                                                             | `HTMLElement` | `undefined` |
+| Property    | Attribute | Description                                                                                                                                   | Type          | Default     |
+| ----------- | --------- | --------------------------------------------------------------------------------------------------------------------------------------------- | ------------- | ----------- |
+| `anchorEl`  | --        | Element to anchor the menu to. If none is given the menu will anchor to the trigger element where aria-controls === menu id                   | `HTMLElement` | `undefined` |
+| `open`      | `open`    | Boolean which controls when to show the menu                                                                                                  | `boolean`     | `false`     |
+| `triggerEl` | --        | Optional element to trigger opening and closing of the menu. If none is supplied the element where aria-controls === menu id will be assigned | `HTMLElement` | `undefined` |
 
 
 ## Events
@@ -26,6 +26,16 @@
 
 ## Methods
 
+### `close() => Promise<boolean>`
+
+Closes the menu. If the menu is already closed it returns 'false'.
+
+#### Returns
+
+Type: `Promise<boolean>`
+
+
+
 ### `isOpen() => Promise<boolean>`
 
 Returns 'true' if the menu is open
@@ -38,7 +48,17 @@ Type: `Promise<boolean>`
 
 ### `show() => Promise<boolean>`
 
-Opens the menu. If the menu is already open it returns 'false.
+Opens the menu. If the menu is already open it returns 'false'.
+
+#### Returns
+
+Type: `Promise<boolean>`
+
+
+
+### `toggle() => Promise<boolean>`
+
+Toggles the menu open or close. Will return 'true' on menu open and 'false' on menu close
 
 #### Returns
 
@@ -49,13 +69,17 @@ Type: `Promise<boolean>`
 
 ## CSS Custom Properties
 
-| Name                         | Description                         |
-| ---------------------------- | ----------------------------------- |
-| `--caretLeft`                | Position of Caret                   |
-| `--caretSize`                | Size of Caret                       |
-| `--popupMenuBackgroundColor` | Pop Up Menu Backround Color         |
-| `--popupMenuBorderColor`     | Pop Up Menu Border Color            |
-| `--transitionSpeed`          | Transition Time of Pop Up Animation |
+| Name                             | Description                             |
+| -------------------------------- | --------------------------------------- |
+| `--caretLeft`                    | Position of Caret                       |
+| `--caretSize`                    | Size of Caret                           |
+| `--menuBackgroundColor`          | Pop Up Menu Backround Color             |
+| `--menuBorderColor`              | Pop Up Menu Border Color                |
+| `--menuItemBackgroundColor`      | Pop Up Menu Item Background Color       |
+| `--menuItemHoverBackgroundColor` | Pop Up Menu Item Hover Background Color |
+| `--menuItemHoverTextColor`       | Pop Up Menu Item Hover Text Color       |
+| `--menuTextColor`                | Pop Up Menu Text Color                  |
+| `--transitionSpeed`              | Transition Time of Pop Up Animation     |
 
 
 ----------------------------------------------
--- src/components/rux-pop-up-menu/rux-pop-up-menu.tsx
+++ src/components/rux-pop-up-menu/rux-pop-up-menu.tsx
@@ -12,6 +12,10 @@ export class RuxPopUpMenu {
 
   @State() _anchorEl: HTMLElement
   
+  /**
+   * Optional element to trigger opening and closing of the menu.
+   * If none is supplied the element where aria-controls === menu id will be assigned
+   */
   @Prop({mutable: true}) triggerEl: HTMLElement
 
   /**
@@ -123,7 +127,7 @@ export class RuxPopUpMenu {
              
     left =
       menuBounds.width + anchorBounds.left - padding > window.innerWidth
-        ? anchorBounds.right - menuBounds.width - padding
+        ? anchorBounds.right - menuBounds.width
         : anchorBounds.left + padding;
 
     top = anchorBounds.bottom + padding / 2 + caret / 2;
@@ -191,164 +195,3 @@ export class RuxPopUpMenu {
   }
 
 }
-
-
-
-//   constructor() {
-//     super();
-
-//     this.data = [];
-//     this.selected = {};
-//     this.expanded = false;
-
-//     this.left = 0;
-//     this.top = 0;
-
-//     this._handleClick = this.handleClick.bind(this);
-//     this._handleOutsideClick = this.handleOutsideClick.bind(this);
-//     this._handleMenuItemClick = this.handleMenuItemClick.bind(this);
-//   }
-
-//   connectedCallback() {
-//     super.connectedCallback();
-//     this._trigger = this.parentElement.querySelector(`[aria-controls="${this.id}"]`);
-//     this._trigger.addEventListener('mousedown', this._handleClick);
-//   }
-
-//   disconnectedCallback() {
-//     this._trigger.removeEventListener('mousedown', this._handleClick);
-//     super.disconnectedCallback();
-//   }
-
-//? Why does this exist? It doesn't appear to be called anywhere
-//   findInObject(arr, key) {
-//     arr.forEach((a) => {
-//       if (a.key === key) {
-//         a.selected = true;
-//         this.found = a;
-//       } else {
-//         delete a.selected;
-//         if (a.children) {
-//           this.findInObject(a.children, key);
-//         }
-//       }
-//     });
-
-//     return this.found;
-//   }
-
-//   handleClick() {
-//     this.show();
-//   }
-
-//   handleOutsideClick(e) {
-//     const target = e
-//         .composedPath()
-//         .find((element) => element.id && element.id === this._trigger.getAttribute('aria-controls'));
-//     target ? this._trigger.addEventListener('mousedown', this._click) : this.hide();
-//   }
-
-//   handleMenuItemClick(e) {
-//     this.selected =  this.data.find((item) => item.id === e.currentTarget.dataset.key);
-//     if(!!this.onPopUpMenuItemSelected){
-//       this.onPopUpMenuItemSelected(this.selected);
-//     }
-//     this.dispatchEvent(
-//         new CustomEvent('pop-up-menu-item-selected', {
-//           detail: {
-//             selected: this.selected,
-//           },
-//           bubbles: true,
-//           composed: true,
-//         })
-//     );
-//     this.hide();
-//   }
-
-//   show() {
-//     this._setMenuPosition();
-//     this.expanded = true;
-//     if(!!this.onPopUpMenuExpandedChange){
-//       this.onPopUpMenuExpandedChange(true);
-//     }
-
-//     const debounce = setTimeout(() => {
-//       window.addEventListener('resize', () => this._setMenuPosition());
-//       window.addEventListener('mousedown', this._handleOutsideClick);
-//       clearTimeout(debounce);
-//     }, 10);
-
-//     this._trigger.removeEventListener('mousedown', this._handleClick);
-
-//     this._menuItems = this.shadowRoot.querySelectorAll('[role="menuitem"]');
-//     this._menuItems.forEach((item) => {
-//       item.addEventListener('mouseup', this._handleMenuItemClick);
-//     });
-//   }
-
-//   hide() {
-//     this.expanded = false;
-//     if(!!this.onPopUpMenuExpandedChange){
-//       this.onPopUpMenuExpandedChange(false);
-//     }
-
-//     window.removeEventListener('mousedown', this._handleOutsideClick);
-//     window.removeEventListener('resize', this);
-
-//     this._menuItems.forEach((item) => {
-//       item.removeEventListener('mouseup', this._handleMenuItemClick);
-//     });
-
-//     this._trigger.addEventListener('mousedown', this._handleClick);
-//   }
-
-//   _setMenuPosition() {
-//     const menuBounds = this.getBoundingClientRect();
-//     const triggerBounds = this._trigger.getBoundingClientRect();
-//     const caret = parseInt(getComputedStyle(this, ':after').height);
-
-//     const padding = 16;
-
-//     this.left =
-//       menuBounds.width + triggerBounds.left - padding > window.innerWidth
-//         ? triggerBounds.right - menuBounds.width
-//         : triggerBounds.left - padding;
-
-//     this.top = triggerBounds.bottom + padding / 2 + caret / 2;
-
-//     if (menuBounds.height + triggerBounds.bottom + padding > window.innerHeight) {
-//       this.top = triggerBounds.top - menuBounds.height - caret;
-//       this.classList.add('from-top');
-//     } else {
-//       this.classList.remove('from-top');
-//     }
-
-//     this.style.left = `${this.left}px`;
-//     this.style.top = `${this.top}px`;
-
-//     const caretLeft = triggerBounds.left - this.left;
-//     this.style.setProperty('--caretLeft', `${caretLeft}px`);
-//   }
-
-//   render() {
-//     const list = this.data.map((item, index) => {
-//       return item.hasOwnProperty('role') && item.role === 'seperator' ? 
-//         html`<li role="seperator"></li>` :
-//         html`<li 
-//                 data-key="${item.hasOwnProperty('id') ? item.id : index }" 
-//                 role="menuitem" 
-//                 tabindex="-1"
-//               >
-//                ${item.label}  
-//              </li>`
-//     });
-
-//     return html`
-//       <ul role="menu" aria-expanded="${this.expanded.toString()}">
-//         ${list}
-//       </ul>
-//       <slot></slot>
-//     `;
-//   }
-
-// }
