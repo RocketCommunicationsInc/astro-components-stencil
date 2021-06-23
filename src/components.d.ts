6aada8bdfbadc0787b6020f84bc26c94b853b670
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-06-23 12:48:52 -0400

    refactor: notification ts strict warnings

--- src/components.d.ts
+++ src/components.d.ts
@@ -10692,7 +10692,7 @@ export namespace Components {
         /**
          * If provided, the banner will automatically close after this amount of time. Accepts value either in milliseconds or seconds (which will be converted to milliseconds internally), between `2000` and `10000`, or `2` and `10`, respectively. Any number provided outside of the `2000`-`10000` range will be ignored in favor of the default 2000ms delay. <br>If `closeAfter` is not passed or if it is given an undefined or `null` value, the banner will stay open until the user closes it.
          */
-        closeAfter: number
+        closeAfter?: number
         /**
          * Message for the notification banner.
          */
