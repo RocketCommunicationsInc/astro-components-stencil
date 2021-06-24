a5b2ef25a0c26fdd03a005c2ee9faa7539d0dfdf
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-06-24 09:56:49 -0400

    build: add dist-custom-element output

--- stencil.config.ts
+++ stencil.config.ts
@@ -15,6 +15,9 @@ export const config: Config = {
             type: 'dist-custom-elements-bundle',
         },
         {
+            type: 'dist-custom-elements',
+        },
+        {
             type: 'docs-readme',
         },
         {
