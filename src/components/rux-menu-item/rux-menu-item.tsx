5d9993c1085273a61be2ce7e67884c32913b6371
Joel Perez <joel.perez@rocketcom.com>
2021-06-30 10:11:04 -0600

    added anchor tag functionality

--- src/components/rux-menu-item/rux-menu-item.tsx
+++ src/components/rux-menu-item/rux-menu-item.tsx
@@ -13,8 +13,12 @@ export class RuxMenuItem {
    */
   @Event() menuItemClicked: EventEmitter<object>
   private itemOnClick = () => {
-    if (this.value) {
-      this.menuItemClicked.emit({value: this.value})
+    if (!this.href) {
+      if (this.value) {
+        this.menuItemClicked.emit({value: this.value})
+      } else {
+        this.menuItemClicked.emit({value: this.el.textContent})
+      }
     } else {
       this.menuItemClicked.emit({value: this.el.textContent})
     }
@@ -30,16 +34,53 @@ export class RuxMenuItem {
    */
   @Prop() value: any
 
+  /**
+   * Contains a URL or a URL fragment that the hyperlink points to.
+   * If this property is set, an anchor tag will be rendered.
+   */
+  @Prop() href: string | undefined;
+
+  /**
+   * Specifies where to display the linked URL.
+   * Only applies when an `href` is provided.
+   * Special keywords: `"_blank"`, `"_self"`, `"_parent"`, `"_top"`.
+   */
+  @Prop() target: string | undefined;
+
+  /**
+   * Specifies the relationship of the target object to the link object.
+   * The value is a space-separated list of [link types](https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types).
+   */
+  @Prop() rel: string | undefined;
+
+  /**
+   * This attribute instructs browsers to download a URL instead of navigating to
+   * it, so the user will be prompted to save it as a local file. If the attribute
+   * has a value, it is used as the pre-filled file name in the Save prompt
+   * (the user can still change the file name if they want).
+   */
+  @Prop() download: string | undefined;
+
   render() {
+    const {disabled, href, rel, download, target, itemOnClick} = this
+    const TagType = href ? 'a' : 'div'
+    const attributes = (TagType === 'a') 
+    ? {download, href, rel, target}
+    : {}
+
     return (
       <Host
-        aria-disabled={this.disabled ? 'true' : null}
+        aria-disabled={disabled ? 'true' : null}
       >
         <li
-          onClick={() => this.itemOnClick()}
+          onClick={() => itemOnClick()}
         >
-          <slot name="start"></slot>
-          <slot></slot>
+          <TagType
+            {...attributes}
+          >
+            <slot name="start"></slot>
+            <slot></slot>
+          </TagType>
         </li>
       </Host>
     );
