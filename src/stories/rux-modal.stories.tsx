cdc3f951eb349a13e5bd20f4cb478af773e41c29
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2021-07-12 13:43:06 -0600

    update modal implementation

--- src/stories/rux-modal.stories.tsx
+++ src/stories/rux-modal.stories.tsx
@@ -5,30 +5,28 @@ import { withActions } from '@storybook/addon-actions'
 import readme from '../components/rux-modal/readme.md'
 
 export default {
-    title: 'Components/Dialog Box',
+    title: 'Components/Modal',
     decorators: [withActions('rux-modal-closed', { depth: 100 }), withKnobs],
 }
 
-export const DialogBox = () => {
+export const Confirmation = () => {
     const toggleModal = boolean('Enable modal', true)
     const message = text('Modal message', 'Modal message')
     const title = text('Modal title', 'Modal title')
     const confirmText = text('Confirm button text', 'Release')
     const denyText = text('Deny button text', 'Cancel')
     return html`
-        <div style="display: flex; flex-flow: column; justify-content: center;">
-            <rux-modal
-                ?open="${toggleModal}"
-                modal-message="${message}"
-                modal-title="${title}"
-                confirm-text="${confirmText}"
-                deny-text="${denyText}"
-            ></rux-modal>
-        </div>
+        <rux-modal
+            ?open="${toggleModal}"
+            modal-message="${message}"
+            modal-title="${title}"
+            confirm-text="${confirmText}"
+            deny-text="${denyText}"
+        ></rux-modal>
     `
 }
 
-DialogBox.parameters = {
+Confirmation.parameters = {
     exports: {
         render,
         html,
