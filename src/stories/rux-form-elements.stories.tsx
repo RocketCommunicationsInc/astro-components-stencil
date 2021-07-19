5e43a16d12bd22b7db2e6db0b5953f876c4e5b56
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-07-19 10:27:46 -0400

    docs(rux-input-field): rename input in regression story

--- src/stories/rux-form-elements.stories.tsx
+++ src/stories/rux-form-elements.stories.tsx
@@ -1,6 +1,6 @@
 import { html, render } from 'lit-html'
 
-import readme from '../../src/components/rux-input/readme.md'
+import readme from '../../src/components/rux-input-field/readme.md'
 
 export default {
     title: 'Components/Form Elements',
@@ -41,92 +41,92 @@ export const InputFields = () => html`
     <div style="padding: 10vh 5vw; display: flex; flex-flow: column; justify-content: center;">
       <ul class="rux-form">
         <li>
-          <rux-input placeholder="Text input" label="Text input"></rux-input>
+          <rux-input-field placeholder="Text input" label="Text input"></rux-input-field>
         </li>
         <li>
-          <rux-input placeholder="Number Input" label="Number input" type="number></rux-input>
+          <rux-input-field placeholder="Number Input" label="Number input" type="number></rux-input-field>
         </li>
         <li>
-          <rux-input placeholder="Text input" label="Text input"></rux-input>
+          <rux-input-field placeholder="Text input" label="Text input"></rux-input-field>
         </li>
         <li>
-          <rux-input placeholder="Text input" label="Text input" help-text="Help text"></rux-input>
+          <rux-input-field placeholder="Text input" label="Text input" help-text="Help text"></rux-input-field>
         </li>
         <li>
-          <rux-input placeholder="Text input" label="Text input" help-text="Help text"></rux-input>
+          <rux-input-field placeholder="Text input" label="Text input" help-text="Help text"></rux-input-field>
         </li>
         <li>
-          <rux-input value=" " label="Is Required" required></rux-input>
+          <rux-input-field value=" " label="Is Required" required></rux-input-field>
         </li>
         <li>
-          <rux-input label="Invalid" error-text="Error text" invalid="true"></rux-input>
+          <rux-input-field label="Invalid" error-text="Error text" invalid="true"></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input label="Disabled" disabled></rux-input>
+          <rux-input-field label="Disabled" disabled></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input label="Password" type="password" placeholder="********"></rux-input>
+          <rux-input-field label="Password" type="password" placeholder="********"></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input label="Invalid password" error-text="Error text" type="password" invalid placeholder="********"></rux-input>
+          <rux-input-field label="Invalid password" error-text="Error text" type="password" invalid placeholder="********"></rux-input-field>
         </li>
         <li>
-          <rux-input label="Web address" type="url" placeholder="https://domain.com"></rux-input>
+          <rux-input-field label="Web address" type="url" placeholder="https://domain.com"></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input type="email" label="Email address" placeholder="user@domain.com"></rux-input>
+          <rux-input-field type="email" label="Email address" placeholder="user@domain.com"></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input type="tel" placeholder="(999) 999-9999" label="Phone number"></rux-input>
+          <rux-input-field type="tel" placeholder="(999) 999-9999" label="Phone number"></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input label="Search" type="search" placeholder="Enter search term"></rux-input>
+          <rux-input-field label="Search" type="search" placeholder="Enter search term"></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input label="Invalid search" type="search" invalid placeholder="Enter search term" error-text="Error text" required>
+          <rux-input-field label="Invalid search" type="search" invalid placeholder="Enter search term" error-text="Error text" required>
         </li>
         <li class="rux-form-field">
-          <rux-input label="Number input" type="number" min="0" max="10" placeholder="Enter a number between 0 and 10"></rux-input>
+          <rux-input-field label="Number input" type="number" min="0" max="10" placeholder="Enter a number between 0 and 10"></rux-input-field>
         </li>
         <li class="rux-form-field">
-          <rux-input label="Invalid number" type="number" min="0" max="10" invalid error-text="Error text" placeholder="Enter a number between 0 and 10"></rux-input>
+          <rux-input-field label="Invalid number" type="number" min="0" max="10" invalid error-text="Error text" placeholder="Enter a number between 0 and 10"></rux-input-field>
         </li>
         <li class="text-area-field">
           <label for="input__textarea">Textarea</label>
           <textarea
             id="input__textarea"
-            class="rux-input"
+            class="rux-input-field"
             placeholder="Multiline text is ok"
           ></textarea>
         </li>
         <!-- Styles for these HTML5 input types still need to be implemented
     <li class="rux-form-field">
       <label for="input__color">Color input</label>
-      <input class="rux-input" type="color" id="input__color" value="#000000">
+      <input class="rux-input-field" type="color" id="input__color" value="#000000">
     </li>
     <li class="rux-form-field">
       <label for="input__range">Range input</label>
-      <input class="rux-input" type="range" id="input__range" value="10">
+      <input class="rux-input-field" type="range" id="input__range" value="10">
     </li>
     <li class="rux-form-field">
       <label for="input__date">Date input</label>
-      <input class="rux-input" type="date" id="input__date" value="1970-01-01">
+      <input class="rux-input-field" type="date" id="input__date" value="1970-01-01">
     </li>
     <li class="rux-form-field">
       <label for="input__month">Month input</label>
-      <input class="rux-input" type="month" id="input__month" value="1970-01">
+      <input class="rux-input-field" type="month" id="input__month" value="1970-01">
     </li>
     <li class="rux-form-field">
       <label for="input__week">Week input</label>
-      <input class="rux-input" type="week" id="input__week" value="1970-W01">
+      <input class="rux-input-field" type="week" id="input__week" value="1970-W01">
     </li>
     <li class="rux-form-field">
       <label for="input__datetime">Datetime input</label>
-      <input class="rux-input" type="datetime" id="input__datetime" value="1970-01-01T00:00:00Z">
+      <input class="rux-input-field" type="datetime" id="input__datetime" value="1970-01-01T00:00:00Z">
     </li>
     <li class="rux-form-field">
       <label for="input__datetime-local">Datetime-local input</label>
-      <input class="rux-input" type="datetime-local" id="input__datetime-local" value="1970-01-01T00:00">
+      <input class="rux-input-field" type="datetime-local" id="input__datetime-local" value="1970-01-01T00:00">
     </li>
     <li class="rux-form-field">
       <div class="rux-button-group">
@@ -151,43 +151,43 @@ export const InputFields = () => html`
       </ul>
       <ul class="rux-form">
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller text input" placeholder="Text input" small></rux-input>
+          <rux-input-field label="Smaller text input" placeholder="Text input" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller number input" type="number" placeholder="Number Input" small></rux-input>
+          <rux-input-field label="Smaller number input" type="number" placeholder="Number Input" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller Is Required" required small></rux-input>
+          <rux-input-field label="Smaller Is Required" required small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller invalid" invalid error-text="Error text" small></rux-input>
+          <rux-input-field label="Smaller invalid" invalid error-text="Error text" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller disabled" disabled small></rux-input>
+          <rux-input-field label="Smaller disabled" disabled small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller password" type="password" placeholder="********" small></rux-input>
+          <rux-input-field label="Smaller password" type="password" placeholder="********" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller web address" type="url" placeholder="https://domain.com" small></rux-input>
+          <rux-input-field label="Smaller web address" type="url" placeholder="https://domain.com" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller email address" placeholder="user@domain.com" type="email" small></rux-input>
+          <rux-input-field label="Smaller email address" placeholder="user@domain.com" type="email" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller phone number" type="tel" placeholder="(999) 999-9999" small></rux-input>
+          <rux-input-field label="Smaller phone number" type="tel" placeholder="(999) 999-9999" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller search" type="search" placeholder="Enter search term" small></rux-input>
+          <rux-input-field label="Smaller search" type="search" placeholder="Enter search term" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
-          <rux-input label="Smaller number input" type="number" min="0" max="10" placeholder="Enter a number between 0 and 10" small></rux-input>
+          <rux-input-field label="Smaller number input" type="number" min="0" max="10" placeholder="Enter a number between 0 and 10" small></rux-input-field>
         </li>
         <li class="rux-form-field rux-form-field--small">
           <label for="input__textarea-smaller">Smaller textarea</label>
           <textarea
             id="input__textarea-smaller"
-            class="rux-input"
+            class="rux-input-field"
             placeholder="Multiline text is ok"
           ></textarea>
         </li>
