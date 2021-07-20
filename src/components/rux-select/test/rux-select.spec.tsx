51d917b89a252340dce41cf142cb896431c41e95
Joel Perez <joel.perez@rocketcom.com>
2021-07-20 11:01:05 -0600

    added render unit test

--- src/components/rux-select/test/rux-select.spec.tsx
+++ src/components/rux-select/test/rux-select.spec.tsx
@@ -5,13 +5,62 @@ describe('rux-select', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [RuxSelect],
-      html: `<rux-select></rux-select>`,
+      html: `
+        <rux-select label="Select an Option">
+          <option value="" selected>Select an Option</option>
+          <optgroup label="Group one">
+              <option>Option 1.1</option>
+              <option>Option 1.2</option>
+              <option>Option 1.3</option>
+              <option>Option 1.4</option>
+          </optgroup>
+          <optgroup label="Group two">
+              <option>Option 2.1</option>
+              <option>Option 2.2</option>
+              <option>Option 2.3</option>
+              <option>Option 2.4</option>
+          </optgroup>
+        </rux-select>
+      `,
     });
     expect(page.root).toEqualHtml(`
-      <rux-select>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
+     <rux-select label="Select an Option">
+       <label>
+         Select an Option
+       </label>
+       <select class="rux-select">
+         <option selected="" value="">
+           Select an Option
+         </option>
+         <optgroup label="Group one">
+           <option>
+             Option 1.1
+           </option>
+           <option>
+             Option 1.2
+           </option>
+           <option>
+             Option 1.3
+           </option>
+           <option>
+             Option 1.4
+           </option>
+         </optgroup>
+         <optgroup label="Group two">
+           <option>
+             Option 2.1
+           </option>
+           <option>
+             Option 2.2
+           </option>
+           <option>
+             Option 2.3
+           </option>
+           <option>
+             Option 2.4
+           </option>
+         </optgroup>
+       </select>
       </rux-select>
     `);
   });
