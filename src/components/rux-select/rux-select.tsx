9579f232cb5c70aea2af2d5eace2807772f90845
Joel Perez <joel.perez@rocketcom.com>
2021-07-20 15:28:25 -0600

    bug: label-id attribute and cleaned up next story

--- src/components/rux-select/rux-select.tsx
+++ src/components/rux-select/rux-select.tsx
@@ -1,55 +1,53 @@
-import { Component, Host, h, Prop, Element } from '@stencil/core';
+import { Component, Host, h, Prop, Element } from '@stencil/core'
 
 @Component({
-  tag: 'rux-select',
-  styleUrl: 'rux-select.scss',
-  scoped: true,
+    tag: 'rux-select',
+    styleUrl: 'rux-select.scss',
+    scoped: true,
 })
 export class RuxSelect {
-  @Element() el!: HTMLRuxSelectElement
+    @Element() el!: HTMLRuxSelectElement
 
-  /**
-    * Disables the item
-    */
-  @Prop({ reflect: true }) disabled: boolean = false
+    /**
+     * Disables the item
+     */
+    @Prop({ reflect: true }) disabled: boolean = false
 
-  /**
-    * Sets the field as required
-   */
-  @Prop({reflect: true}) required: boolean = false
+    /**
+     * Sets the field as required
+     */
+    @Prop({ reflect: true }) required: boolean = false
 
-  /**
-    * Sets the Label for the Select
-   */
-  @Prop() label?: string
+    /**
+     * Sets the Label for the Select
+     */
+    @Prop() label?: string
 
-  /**
-   * Id for the Select Input
-   */
-  @Prop({attribute: 'input-id'}) inputId?: string
+    /**
+     * Id for the Select Input
+     */
+    @Prop({ attribute: 'input-id' }) inputId?: string
 
-  /**
-   * Id for the Label
-   */
-  @Prop({attribute: 'input-id'}) labelId?: string
+    /**
+     * Id for the Label
+     */
+    @Prop({ attribute: 'label-id' }) labelId?: string
 
-
-  render() {
-    const {disabled, required, label, inputId, labelId} = this
-    return (
-      <Host>
-        {label && <label id={labelId}>{label}</label>}
-        <select
-          class="rux-select"
-          id={inputId}
-          aria-labelledby={labelId}
-          disabled={disabled}
-          required={required}
-        >
-          <slot></slot>
-        </select>
-      </Host>
-    );
-  }
-
+    render() {
+        const { disabled, required, label, inputId, labelId } = this
+        return (
+            <Host>
+                {label && <label id={labelId}>{label}</label>}
+                <select
+                    class="rux-select"
+                    id={inputId}
+                    aria-labelledby={labelId}
+                    disabled={disabled}
+                    required={required}
+                >
+                    <slot></slot>
+                </select>
+            </Host>
+        )
+    }
 }
