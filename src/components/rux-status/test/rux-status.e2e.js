25fe2dc04df82067ba8c716fd94b00cd56882926
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-07-23 15:15:05 -0400

    fix: failing status test

--- src/components/rux-status/test/rux-status.e2e.js
+++ src/components/rux-status/test/rux-status.e2e.js
@@ -1,6 +1,6 @@
 describe('Status', () => {
     beforeEach(() => {
-        cy.visitStory('components-status--default-·story')
+        cy.visitStory('components-status--default-story')
     })
     it('renders', async () => {
         cy.get('rux-status').should('have.class', 'hydrated')
