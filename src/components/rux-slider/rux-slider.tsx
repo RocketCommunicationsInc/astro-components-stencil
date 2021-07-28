81282e4ffcc3e6cf26ad61f1f194ffa2ae4a96aa
Micah Jones <micah.jones@rocketcom.com>
2021-07-28 12:46:24 -0600

    feat: added watch to val min max, updated css vars

--- src/components/rux-slider/rux-slider.tsx
+++ src/components/rux-slider/rux-slider.tsx
@@ -6,6 +6,7 @@ import {
     Element,
     Event,
     EventEmitter,
+    Watch,
 } from '@stencil/core'
 
 @Component({
@@ -49,6 +50,14 @@ export class RuxSlider {
         this.onInput = this.onInput.bind(this)
     }
     @Element() el!: HTMLInputElement
+
+    @Watch('val')
+    @Watch('min')
+    @Watch('max')
+    handleChange() {
+        this._getValueOnLoad()
+    }
+
     _getValueOnLoad() {
         //If val is not a number, change it to 0.
         if (!this.val && this.val != 0) {
@@ -59,24 +68,34 @@ export class RuxSlider {
             this.min = 0
         }
         //Min can't be > max
-        if (this.max && this.min > this.max) {
-            this.min = this.max - 5
+        if (this.min > this.max!) {
+            console.log('made it')
+            if (this.step && this.step != 0) {
+                this.min = this.max! - this.step!
+            } else {
+                this.min = this.max! - 5
+            }
         }
         //If max is not a number, change it to 100
         if (!this.max && this.max != 0) {
             this.max = 100
         }
         // If min is given and is greater than value, then set value to the min.
-        if (this.val && this.val < this.min!) {
+        if (this.val && this.val < this.min) {
             this.val = this.min
         }
         //If max is given and is less than value, set value to max
         if (this.max && this.max < this.val) {
-            console.log('change val')
             this.val = this.max
         }
+
         this.el.style.setProperty('--value', this.val!.toString())
         this._setValuePercent()
+        console.log(this.max, 'max')
+        console.log(this.min, 'min')
+        console.log(this.val, 'val')
+        console.log(this.step, 'step')
+        console.log(this.disabled, 'disabled')
     }
     //Sets the --valuePercent CSS var
     _setValuePercent() {
@@ -107,6 +126,7 @@ export class RuxSlider {
                         step={step}
                         disabled={disabled}
                         aria-label="slider"
+                        aria-disabled={disabled ? 'true' : 'false'}
                     ></input>
                 </div>
                 <slot></slot>
