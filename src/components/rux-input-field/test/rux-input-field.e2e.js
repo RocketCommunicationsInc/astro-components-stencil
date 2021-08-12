d52b27140bebb39605e630975ba98a09fe500a5d
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2021-08-12 15:29:21 -0600

    write form field e2e tests

--- src/components/rux-input-field/test/rux-input-field.e2e.js
+++ src/components/rux-input-field/test/rux-input-field.e2e.js
@@ -6,3 +6,81 @@ describe('Input Field', () => {
         cy.get('rux-input-field').should('have.class', 'hydrated')
     })
 })
+
+describe('Input Field with Form', () => {
+    const testString = 'Hello World'
+
+    beforeEach(() => {
+        cy.visit('localhost:4444/tests/pages/form-input-field.html')
+    })
+
+    it('submits the correct value when using a form', async () => {
+        cy.get('#ruxInput').shadow().find('input').type(testString)
+        cy.get('#nativeInput').type(testString)
+
+        cy.get('#form').submit()
+
+        cy.get('#log').children().its('length').should('eq', 2)
+
+        cy.get('#log').contains(`ruxInput:${testString}`)
+        cy.get('#log').contains(`nativeInput:${testString}`)
+    })
+
+    it('does not submit disabled even with value', async () => {
+        cy.get('#ruxInput2')
+            .shadow()
+            .find('input')
+            .invoke('val')
+            .should('have.value', testString)
+
+        cy.get('#nativeInput2').should('have.value', testString)
+
+        cy.get('#ruxInput2').should('be.disabled')
+        cy.get('#ruxInput2').shadow().find('input').should('be.disabled')
+        cy.get('#nativeInput2').should('be.disabled')
+
+        cy.get('#form').submit()
+
+        cy.get('#log').children().its('length').should('eq', 2)
+
+        cy.get('#log').contains(`ruxInput:${testString}`)
+        cy.get('#log').contains(`nativeInput:${testString}`)
+    })
+
+    it('does not submit a value if input is cleared', () => {
+        cy.get('#ruxInput')
+            .shadow()
+            .find('input')
+            .type(testString)
+            .should('have.value', testString)
+        cy.get('#nativeInput').type(testString).should('have.value', testString)
+
+        cy.get('#ruxInput').shadow().find('input').clear()
+        cy.get('#nativeInput').clear()
+
+        cy.get('#form').submit()
+        cy.get('#log').should('not.contain', `ruxInput:${testString}`)
+        cy.get('#log').should('not.contain', `nativeInput:${testString}`)
+    })
+
+    it('passes correct label', () => {
+        cy.get('#ruxInput')
+            .shadow()
+            .find('.rux-input-label')
+            .contains('Input Field')
+    })
+
+    it('prepends aesthetics to lable if required', () => {
+        cy.get('#ruxInput3')
+            .shadow()
+            .find('.rux-input-label .rux-input-label__asterisk')
+            .contains('*')
+    })
+
+    it('prepends help text if attribute set', () => {
+        cy.get('#ruxInput3')
+            .shadow()
+            .find('.rux-help-text')
+            .contains('Test Help Text')
+    })
+})
--- src/components/rux-input-field/test/rux-input-field.spec.tsx
+++ src/components/rux-input-field/test/rux-input-field.spec.tsx
@@ -12,7 +12,7 @@ describe('rux-input', () => {
             <mock:shadow-root>
             <div class="rux-form-field">
               <label class="rux-input-label" htmlfor="input-1"></label>
-              <input aria-invalid="false" class="rux-input" id="input-1" type="text" value="">
+              <input aria-invalid="false" class="rux-input" id="rux-input-1" type="text" value="">
             </div>
             </mock:shadow-root>
           </rux-input-field>
