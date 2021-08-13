a007200e23441002b3f6583d224dfc709b61ebe1
NicDominguez <nic.dominguez@rocketcom.com>
2021-08-13 08:58:51 -0700

    test: adds click and enter key close tests

--- src/components/rux-modal/test/rux-modal.e2e.js
+++ src/components/rux-modal/test/rux-modal.e2e.js
@@ -5,4 +5,22 @@ describe('Modal', () => {
     it('renders', () => {
         cy.get('rux-modal').should('have.class', 'hydrated')
     })
+    it('closes when click occurs outside modal', () => {
+        cy.get('rux-modal')
+            .shadow()
+            .find('.rux-modal__wrapper')
+            .click('topLeft')
+        cy.get('rux-modal')
+            .shadow()
+            .find('.rux-modal__wrapper')
+            .should('not.exist')
+    })
+    it('closes when enter key pressed', () => {
+        cy.get('rux-modal').shadow().find('dialog').click()
+        cy.get('body').type('{enter}')
+        cy.get('rux-modal')
+            .shadow()
+            .find('.rux-modal__wrapper')
+            .should('not.exist')
+    })
 })
