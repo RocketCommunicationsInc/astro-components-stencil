5b86886b43c443146be55aa2f1f554b6a9339a08
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2021-08-24 12:57:33 -0600

    add angular wrapper settings

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,11 +1,46 @@
 import { Config } from '@stencil/core'
 import { sass } from '@stencil/sass'
 import { svgOptimizerPlugin } from './src/utils/rollup-svg'
+import {
+    angularOutputTarget,
+    ValueAccessorConfig,
+} from '@stencil/angular-output-target'
+
+// const EVENTS = {
+//     Input: 'onInput',
+//     Change: 'onChange',
+// }
+
+// const ATTRS = {
+//     Checked: 'checked',
+//     Value: 'value',
+// }
+
+// const angularValueAccessorBindings: ValueAccessorConfig[] = [
+//     {
+//         elementSelectors: ['rux-checkbox', 'rux-switch'],
+//         event: EVENTS.Input,
+//         targetAttr: ATTRS.Value,
+//         type: 'boolean',
+//     },
+//     {
+//         elementSelectors: ['rux-checkbox', 'rux-switch'],
+//         event: EVENTS.Change,
+//         targetAttr: ATTRS.Checked,
+//         type: 'boolean',
+//     },
+// ]
 
 export const config: Config = {
     namespace: 'astro-web-components',
     globalStyle: 'src/global/global.scss',
     outputTargets: [
+        angularOutputTarget({
+            componentCorePackage: '@astrouxds/astro-web-components',
+            directivesProxyFile:
+                '../astro-components-angular/src/proxies/astro-web-components.ts',
+            valueAccessorConfigs: [],
+        }),
         {
             type: 'dist',
             esmLoaderPath: '../loader',
