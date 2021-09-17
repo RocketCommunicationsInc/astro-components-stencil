f5b61cfcfa7e6c26c13d52dcaa7e2d0489784322
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-09-17 11:11:15 -0400

    test(rux-slider): update unit test

--- src/components/rux-slider/test/rux-slider.spec.tsx
+++ src/components/rux-slider/test/rux-slider.spec.tsx
@@ -10,8 +10,10 @@ describe('rux-slider', () => {
         expect(page.root).toEqualHtml(`
       <rux-slider style="--slider-value-percent: 50%;">
         <mock:shadow-root>
-        <div class="rux-slider">
-           <input aria-disabled="false" aria-label="slider" class="rux-range" max="100" min="0" step="1" type="range" value="50">
+          <div class="rux-form-field">
+            <div class="rux-slider">
+              <input aria-disabled="false" aria-label="slider" id="rux-slider-1" class="rux-range" max="100" min="0" step="1" type="range" value="50">
+            </div>
          </div>
          <slot></slot>
         </mock:shadow-root>
