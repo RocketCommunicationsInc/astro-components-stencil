f2c38297538c64086d5f4291bda58bd2f410f0a1
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-09-28 14:29:51 -0400

    test(rux-select): fix unit test

--- src/components/rux-select/test/rux-select.spec.tsx
+++ src/components/rux-select/test/rux-select.spec.tsx
@@ -6,132 +6,35 @@ describe('rux-select', () => {
         const page = await newSpecPage({
             components: [RuxSelect],
             html: `
-        <rux-select label="Select an Option">
-          <option value="" selected>Select an Option</option>
-          <optgroup label="Group one">
-              <option>Option 1.1</option>
-              <option>Option 1.2</option>
-              <option>Option 1.3</option>
-              <option>Option 1.4</option>
-          </optgroup>
-          <optgroup label="Group two">
-              <option>Option 2.1</option>
-              <option>Option 2.2</option>
-              <option>Option 2.3</option>
-              <option>Option 2.4</option>
-          </optgroup>
-        </rux-select>
+            <rux-select label="test">
+            <option>one</option>
+            <option>two</option>
+            </rux-select>
       `,
         })
         expect(page.root).toEqualHtml(`
-     <rux-select label="Select an Option">
-       <label aria-hidden="false">
-        <span>
-          <slot-fb name="label">
-            Select an Option
-          </slot>
-        </span>
-       </label>
-       <select class="rux-select">
-         <option selected="" value="">
-           Select an Option
-         </option>
-         <optgroup label="Group one">
-           <option>
-             Option 1.1
-           </option>
-           <option>
-             Option 1.2
-           </option>
-           <option>
-             Option 1.3
-           </option>
-           <option>
-             Option 1.4
-           </option>
-         </optgroup>
-         <optgroup label="Group two">
-           <option>
-             Option 2.1
-           </option>
-           <option>
-             Option 2.2
-           </option>
-           <option>
-             Option 2.3
-           </option>
-           <option>
-             Option 2.4
-           </option>
-         </optgroup>
-       </select>
-      </rux-select>
-    `)
-    })
-
-    it('renders label slot', async () => {
-        const page = await newSpecPage({
-            components: [RuxSelect],
-            html: `
-      <rux-select>
-        <div slot="label">hello</div>
-        <option value="" selected>Select an Option</option>
-        <optgroup label="Group one">
-            <option>Option 1.1</option>
-            <option>Option 1.2</option>
-            <option>Option 1.3</option>
-            <option>Option 1.4</option>
-        </optgroup>
-        <optgroup label="Group two">
-            <option>Option 2.1</option>
-            <option>Option 2.2</option>
-            <option>Option 2.3</option>
-            <option>Option 2.4</option>
-        </optgroup>
-      </rux-select>
-    `,
-        })
-        expect(page.root).toEqualHtml(`
-   <rux-select>
-     <label aria-hidden="false">
-      <span>
-        <div slot="label">hello</div>
-      </span>
-     </label>
-     <select class="rux-select">
-       <option selected="" value="">
-         Select an Option
-       </option>
-       <optgroup label="Group one">
-         <option>
-           Option 1.1
-         </option>
-         <option>
-           Option 1.2
-         </option>
-         <option>
-           Option 1.3
-         </option>
-         <option>
-           Option 1.4
-         </option>
-       </optgroup>
-       <optgroup label="Group two">
-         <option>
-           Option 2.1
-         </option>
-         <option>
-           Option 2.2
-         </option>
-         <option>
-           Option 2.3
-         </option>
-         <option>
-           Option 2.4
-         </option>
-       </optgroup>
-     </select>
-    </rux-select>
-  `)
+          <rux-select label="test">
+            <mock:shadow-root>
+                <label aria-hidden="false">
+                    <span>
+                        <slot name="label">
+                            test
+                        </slot>
+                    </span>
+                </label>
+                <select class="rux-select"></select>
+                <div aria-hidden="true" class="hidden">
+                    <slot></slot>
+                </div>
+            </mock:shadow-root>
+              <option>
+                  one
+              </option>
+              <option>
+                  two
+              </option>
+              <input class="aux-input" type="hidden" value="">
+          </rux-select>
+      `)
     })
 })
